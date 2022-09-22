874980562dd82ff112e0a9c98fb912958313d82e
wchizewski <90284175+wchizewski@users.noreply.github.com>
2022-09-22 14:39:27 -0600

    thurdfay

--- functions.js
+++ functions.js
@@ -1,1 +1,26 @@
 // DEFINE YOUR FUNCTIONS HERE BASED ON THE PROVIDED CRITERIA
+
+function includes(item, array) {
+  let trueOrFalse = false;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === item) {
+      trueOrFalse = true;
+    }
+  }
+  return trueOrFalse;
+}
+
+function indexOf(item, array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === item) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+function reverse(array) {
+    for (let i = 0; i < array.length; i++) {
+        
+    }
+}
