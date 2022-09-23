69ca4c9b53f62cb4648137a3d1abbb3c9c90d846
wchizewski <90284175+wchizewski@users.noreply.github.com>
2022-09-23 14:45:18 -0600

    done!

--- functions.js
+++ functions.js
@@ -20,8 +20,44 @@ function indexOf(item, array) {
 }
 
 function reverse(array) {
-    let reverse = []
-    for (let i = 0; i < array.length; i++) {
-        array[i] 
+    let reversed = [];
+    for (let i = array.length - 1; i >= 0; i--) {
+        reversed.push(array[i]);
     }
+    return reversed;
+}
+
+function slice(start, stop, array) {
+  let sliced = [];
+  for (let i = start; i < array.length; i++) {
+    if (i < stop){
+      sliced.push(array[i]);
+    }
+  }
+  return sliced;
+}
+
+function concat(array1, array2) {
+  for (let i = 0; i < array2.length; i++) {
+    array1.push(array2[i]);
+  }
+  return array1;
+}
+
+function join(sep, array) {
+  let string = array[0];
+  for (let i = 1; i < array.length; i++) {
+    string += `${sep}${array[i]}`;
+  }
+  return string;
+}
+
+function max(array) {
+  let maxNum = array[0]; 
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > maxNum) {
+      maxNum = array[i]
+    }
+  }
+  return maxNum;
 }
